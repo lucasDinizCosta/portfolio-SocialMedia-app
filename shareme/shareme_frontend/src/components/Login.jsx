a3d5c88bec906c73b04f234090c56ce981c88c84
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2022-01-19 12:17:03 -0300

    Add video on background of the Login Component

--- shareme/shareme_frontend/src/components/Login.jsx
+++ shareme/shareme_frontend/src/components/Login.jsx
@@ -3,10 +3,23 @@ import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className="realtive w-full h-full">
+                <video 
+                    src={shareVideo}
+                    type="video/mp4"
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className="w-full h-full object-cover"
+                />
+            </div>
         </div>
     )
 }
